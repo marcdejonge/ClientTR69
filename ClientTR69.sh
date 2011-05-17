5031dbf36787a5c00092e673dbd105e1a8f9b4b6
obeyler <obeyler@e6fce60f-1bb4-4b06-9e73-886240b3b755>
2011-05-17 20:00:08 +0000

    remove old file
    git-svn-id: https://modus-tr-069.svn.sourceforge.net/svnroot/modus-tr-069/trunk/ClientTR69@3 e6fce60f-1bb4-4b06-9e73-886240b3b755

--- ClientTR69.sh
+++
@@ -1,1 +0,0 @@
-java -Dhttp.nonProxyHosts="localhost" -Dhttp.proxyHost=goodway -Dhttp.proxyPort=3128 -jar framework.jar 
---
+++ src/main/resources/ClientTR69.sh
@@ -0,0 +1,3 @@
+#java -Dhttp.nonProxyHosts="localhost" -Dhttp.proxyHost=xxxxx -Dhttp.proxyPort=3128 -jar framework.jar 
+
+java -jar framework.jar 
